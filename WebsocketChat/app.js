15162603664a11ed8970bd9e8df0cd736989d611
HToy91 <jfvargasfigueroa@email.neit.edu>
2026-02-11 22:23:31 -0500

    Add WebsocketChat initial project files
    Add a new WebsocketChat project: includes an Express + Socket.IO based chat server (app.js) that serves a static public folder, handles usernames, broadcasts chat messages and maintains a user list. Also adds package.json and package-lock.json with dependencies (express, socket.io, ws), a basic ReadMe with setup commands, public/index.html, .gitignore and IDE configuration files under .idea. This provides the initial project skeleton to run and develop the websocket chat app.

---
+++ WebsocketChat/app.js
@@ -0,0 +1,48 @@
+const {socket} = require("dgram"); // This line seems to be unused and can be removed. It imports the 'socket' module from 'dgram', which is not needed in this code.
+const express = require("express"); // Import the Express framework to create a web server
+const app = express(); // Create an instance of the Express application
+const http = require("http"); // Import the built-in 'http' module to create an HTTP server
+const {Server} = require("socket.io"); // Import the 'Server' class from the 'socket.io' module to create a Socket.IO server
+
+const server = http.createServer(app); // This line is duplicated and should be removed. It creates another HTTP server, which is unnecessary.
+const io = new Server(server); // Create a Socket.IO server and attach it to the HTTP server
+
+// Setup static public folder
+app.use(express.static("public"));
+
+const users = new Set(); // Create a Set to store unique usernames. Using a Set ensures that there are no duplicate usernames in the chat application.
+
+io.on("connection", (socket) => {
+    console.log("A user connected"); // Log when a user connects
+
+    // Event for chat message
+    socket.on("chat message", (msg) => {
+        console.log(`Message received: ${msg}`); // Log the received message
+        io.emit("chat message", `${socket.username}: ${msg}`); // Broadcast the message to all connected clients
+    });
+
+    // Event for setting username
+    socket.on("set username", (username) => {
+        socket.username = username;
+        users.add(username);
+        io.emit("user list", Array.from(users));
+    });
+
+    // For disconnection
+    socket.on("disconnect", () => {
+        console.log(`${socket.username} disconnected`); // Log when a user disconnects
+        users.delete(socket.username);
+        if (socket.username) {
+            io.emit("chat message", `${socket.username} has left the chat`); // Broadcast a message that the user has left
+
+        }
+
+        io.emit("user list", Array.from(users));
+    });
+});
+
+
+const port = 3000;
+server.listen(3000, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+}); // Start the server and listen on port 3000
