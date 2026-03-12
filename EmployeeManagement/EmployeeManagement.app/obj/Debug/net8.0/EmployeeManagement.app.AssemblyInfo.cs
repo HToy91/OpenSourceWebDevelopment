59ae1e47a76572685ff68c7009cdb1ec4ae28a89
HToy91 <jfvargasfigueroa@email.neit.edu>
2026-03-12 11:06:35 -0400

    Update assembly informational version
    Update generated AssemblyInfo to new informational version (from 1.0.0+548e7acb... to 1.0.0+8a34c5db...) and refresh the AssemblyInfoInputs.cache hash. These are auto-generated build artifacts and do not change source code logic.

--- EmployeeManagement/EmployeeManagement.app/obj/Debug/net8.0/EmployeeManagement.app.AssemblyInfo.cs
+++ EmployeeManagement/EmployeeManagement.app/obj/Debug/net8.0/EmployeeManagement.app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+548e7acb3fbe6f67a857e65996e77aee78b29ff2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a34c5db9efff755c54aa03cf8a2ea8c57463ead")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
